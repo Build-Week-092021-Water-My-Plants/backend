00bc3e0bf0f480244f487f40de09784779a6d85a
Broveku <gearhearjordan@gmail.com>
2021-09-21 14:50:22 -0700

    wip

--- api/auth/auth-restricted.js
+++ api/auth/auth-restricted.js
@@ -1,11 +1,11 @@
 require("dotenv").config()
 const jwt = require("jsonwebtoken")
-const jwtSecret = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET || 'fan of the game'
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (token) {
-    jwt.verify(token, jwtSecret, (err, decoded) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
         res.status(400).json({ 
           message: "token invalid", 
