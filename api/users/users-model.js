69f00e0d0b5f5e1bf33fb6c10097734094fcad73
Broveku <gearhearjordan@gmail.com>
2021-09-22 16:17:57 -0700

    refactor users model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,20 +1,40 @@
-const db = require("../data/db-config")
+const db = require('../data/db-config')
 
-function getUsers() {
-    return db("users")
+function get() {
+  return db('users')
 }
 
 function getBy(filter) {
-    return db("users").where(filter).orderBy('user_id')
-  }
+  return db('users').where(filter).orderBy("user_id")
+}
 
-async function createUser(user) {
-    const [id] = await db("users").insert(user)
-    return getUsers().where({ id }).first()
+function getById(user_id) {
+  return db('users').where('user_id', user_id).first()
+}
+
+function getUserPlants(user_id) {
+  return db("users")
+    .leftJoin('plants', 'users.user_id', 'plants.user_id')
+    .where("users.user_id", user_id)
+    .select('h2oFrequency', 'image', 'nickname', 'plant_id', 'species')
+    .orderBy('plant_id')
+}
+
+
+function update(user_id, changes) {
+  return db('users').where('user_id', user_id).first().update(changes)
+}
+
+async function add(newUser) {
+  const [user] = await db('users').insert(newUser, ['user_id', 'username', 'phone_number'])
+  return user
 }
 
 module.exports = {
-    getUsers,
-    getBy,
-    createUser
-} 
+  get,
+  getBy,
+  getById,
+  getUserPlants,
+  add,
+  update,
+}
