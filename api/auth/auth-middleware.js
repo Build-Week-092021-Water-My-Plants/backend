04cc9abea20f64a64f1f660cf907f55eb654dd21
Broveku <gearhearjordan@gmail.com>
2021-09-22 15:54:25 -0700

    auth middleware / router adds

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,19 +1,4 @@
-const Users = require('../users/users-model')
-
-const checkCredentials = (req, res, next)=>{
-    const { username, password } = req.body
-    const valid = Boolean(username && password && typeof password === 'string')
-
-    if (valid) {
-        next()
-    } else {
-        res.status(422).json({
-            message: 'Username and Password please'
-        })
-    }
-}
 const checkUsernameExists = (req, res, next) => {
-    
     const { username } = req.body;
     Users.getBy({ username })
         .then(exists => {
@@ -27,7 +12,64 @@ const checkUsernameExists = (req, res, next) => {
         .catch(next);
 }
 
+function checkUsernameFree(req, res, next) {
+    const { username } = req.body;
+    Users.getBy({ username })
+        .then(exists => {
+            if (exists.length) {
+                res.status(422).json({message: "Username taken"})
+            } else {
+                next();
+            }
+        })
+        .catch(next)
+}
+
+const noMissingInformation = (req, res, next) => {
+    const { username, password, phone_number } = req.body;
+    if (!username || !password || !phone_number) {
+        res.status(422).json({message: "Username, password, and mobile number are required."})
+    } else {
+        next()
+    }
+}
+
+const noMissingCredentials = (req, res, next) => {
+    const { username, password} = req.body;
+    if (!username || !password) {
+        res.status(422).json({message: "Both username and password are required."})
+    } else {
+        next()
+    }
+}
+
+function checkPhoneNumberFree(req, res, next) {
+    const { phone_number } = req.body;
+    Users.getBy({ phone_number })
+        .then(exists => {
+            if (exists.length) {
+                res.status(422).json({message: "Phone number already in use."})
+            } else {
+                next();
+            }
+        })
+        .catch(next)
+}
+
+function checkPhoneNumberLength(req, res, next) {
+    const { phone_number } = req.body;
+    if(phone_number.length !== 10) {
+      res.status(422).json({message: "Please enter a valid phone number (10 digits)."})
+    } else {
+      next();
+    }
+  }
+
 module.exports = {
-    checkCredentials,
-    checkUsernameExists
+    checkUsernameExists,
+    checkUsernameFree,
+    noMissingInformation,
+    noMissingCredentials,
+    checkPhoneNumberFree,
+    checkPhoneNumberLength
 }
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,33 +1,61 @@
-const router = require('express').Router()
-const bcrypt = require('bcryptjs')
-const Users = require('../users/users-model')
-const tokenBuilder = require('./token-builder')
-const { checkCredentials, checkUsernameExists } = require('./auth-middleware')
+const router = require('express').Router();
+const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
+const tokenBuilder = require('./token-builder');
+const { 
+    checkUsernameExists,
+    checkUsernameFree,
+    noMissingInformation,
+    noMissingCredentials,
+    checkPhoneNumberFree,
+    checkPhoneNumberLength 
+} = require('./auth-middleware');
 
 
-router.post('/register', checkCredentials, (req, res, next)=>{
-    let user = req.body
-    const rounds = process.env.BCRYPT_ROUNDS || 8
-    const hash = bcrypt.hashSync(user.password, rounds)
-    user.password = hash
+router.post("/register",
+noMissingInformation,
+checkUsernameFree, checkPhoneNumberLength,
+checkPhoneNumberFree,
+(req, res, next) => {
+    const user = req.body;
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+    const hash = bcrypt.hashSync(user.password, rounds);
+    user.password = hash;
+  
+    Users.add(user)
+      .then(addedUser => {
+        res.status(201).json(addedUser);
+      })
+      .catch(next);
+});
 
-    Users.createUser(user)
-         .then((saved)=>{
-             res.status(201).json({
-                 message: `Welcome!! , ${saved.username}`
-             })
-         })
-         .catch(next)
-})
-
-router.post("/login", checkCredentials, checkUsernameExists, (req, res) => {
+router.post("/login",
+noMissingCredentials,
+checkUsernameExists,
+(req, res) => {
     const { username, password } = req.body;
     if (bcrypt.compareSync(password, req.user.password)) {
         const token = tokenBuilder(req.user)
-        res.status(200).json({message: `Welcome ${username}!`, token})
+        req.session.user = req.user
+        const { user_id } = req.user;
+        res.status(200).json({message: `Welcome ${username}!`, user_id, token})
     } else {
         res.status(401).json({message: "Invalid credentials"})
     }
 }) 
 
-module.exports = router
+router.get("/logout", (req, res, next) => {
+    if (req.session.user) {
+        req.session.destroy(error => {
+          if (error) {
+            res.status(200).json({message: "Error logging out."})
+          } else {
+            res.status(200).json({message: "Logged out."})
+          }
+        });
+      } else {
+        res.status(200).json({message: "There was no session to begin with."})
+    }
+})
+
+module.exports = router;
