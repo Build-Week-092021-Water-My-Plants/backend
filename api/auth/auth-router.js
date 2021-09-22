e347c8f6dac87e91df218af52ae0d7004fd9832d
Broveku <gearhearjordan@gmail.com>
2021-09-22 13:37:48 -0700

    working register and login endpoints

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,13 +2,11 @@ const router = require('express').Router()
 const bcrypt = require('bcryptjs')
 const Users = require('../users/users-model')
 const tokenBuilder = require('./token-builder')
-const checkCredentials = require('./auth-middleware')
+const { checkCredentials, checkUsernameExists } = require('./auth-middleware')
 
 
 router.post('/register', checkCredentials, (req, res, next)=>{
     let user = req.body
-
-
     const rounds = process.env.BCRYPT_ROUNDS || 8
     const hash = bcrypt.hashSync(user.password, rounds)
     user.password = hash
@@ -22,20 +20,15 @@ router.post('/register', checkCredentials, (req, res, next)=>{
          .catch(next)
 })
 
-router.post('/login', checkCredentials, (req, res, next)=>{
-    let { username, password } = req.body
+router.post("/login", checkCredentials, checkUsernameExists, (req, res) => {
+    const { username, password } = req.body;
+    if (bcrypt.compareSync(password, req.user.password)) {
+        const token = tokenBuilder(req.user)
+      console.log(req.body)
+        res.status(200).json({message: `Welcome ${username}!`, token})
+    } else {
+        res.status(401).json({message: "Invalid credentials"})
+    }
+}) 
 
-    Users.getBy({ username }) 
-      .then(([user]) => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          const token = tokenBuilder(user)
-          res.status(200).json({
-            message: `Welcome back ${user.username}!`,
-            token
-          })
-        } else {
-          res.status(401).json({ message: "Invalids" })
-        }
-      })
-      .catch(next)
-  })
+module.exports = router
