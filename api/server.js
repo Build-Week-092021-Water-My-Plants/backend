c752e7b8f11f8cf32b223a244d4d5101e9620098
Broveku <gearhearjordan@gmail.com>
2021-09-22 16:09:39 -0700

    adds to secrets and server.js

--- api/server.js
+++ api/server.js
@@ -1,26 +1,49 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-
-const authRouter = require('./auth/auth-router')
-const usersRouter = require('./users/users-router')
-const plantRouter = require('./plants/plants-router')
-const restricted = require('./auth/auth-restricted')
+const session = require('express-session')
+const store = require('connect-session-knex')(session)
+const authRouter = require("./auth/auth-router.js")
+const usersRouter = require("./users/users-router.js")
+const plantsRouter = require("./plants/plants-router")
 
 const server = express()
+
+server.use(
+  session({
+    name: 'Dopey',
+    secret: '*hiccup*',
+    cookie: {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        secure: false,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+
+    store: new store({
+        knex: require('./data/db-config'),
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        createtable: true,
+        clearInterval: 1000 * 60 * 60,
+  }),
+}))
+
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/plants", plantsRouter);
 
-server.use('/api/auth', authRouter)
-server.use('/api/users', restricted, usersRouter)
-server.use('/api/plants', plantRouter)
-
-
+server.get("/", (req, res) => {
+    res.json({ api: "up" });
+}) 
 
 server.use((err, req, res, next) => { // eslint-disable-line
-    res.status(500).json({
+    res.status(err.status || 500).json({
       message: err.message,
       stack: err.stack,
     })
